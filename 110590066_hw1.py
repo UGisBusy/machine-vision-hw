7ce1a745208e6ef385adf045e5a696be5f7e1967
UG <ug00257793@gmail.com>
2024-03-06 23:05:23 +0800

    indexed color img

--- 110590066_hw1.py
+++ 110590066_hw1.py
@@ -40,11 +40,31 @@ def convert_to_binary(gray_img, threshold):
     new_img = np.zeros_like(gray_img)
     for i in range(gray_img.shape[0]):
         for j in range(gray_img.shape[1]):
-            if gray_img[i, j] > threshold:
-                new_img[i, j] = 255
-            else:
-                new_img[i, j] = 0
+            new_img[i, j] = 255 if gray_img[i, j] > threshold else 0
     return new_img
+
+
+# 1.3 Convert the color image to the index-color image
+def convert_to_index_color(img):
+    # knn to find the 16 colors
+    k = 16
+    data = img.reshape(-1, 3).astype(np.uint8)
+    centroids = data[np.random.choice(range(len(data)), k, replace=False)]
+    for _ in range(100):
+        labels = np.argmin(np.linalg.norm(data - centroids[:, np.newaxis], axis=2), axis=0)
+        new_centroids = np.array(
+            [(data[labels == i].mean(axis=0) if len(data[labels == i]) > 0 else centroids[i]) for i in range(k)]
+        )
+        if np.linalg.norm(new_centroids - centroids) < 1e-4:
+            break
+        centroids = new_centroids
+
+    # assign each pixel to the nearest centroid
+    index_color_img = np.zeros_like(img)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            index_color_img[i, j] = centroids[labels[i * img.shape[1] + j]]
+    return index_color_img
 
 
 # 2.1 Resize the image without interpolation
@@ -77,6 +97,10 @@ if __name__ == "__main__":
         binary_img = convert_to_binary(gray_img, (81, 147, 70)[img_id])
         # binary_img = convert_to_binary_trackbar(gray_img)
         cv2.imwrite(os.path.join(results_dir, filename[:-4] + "_q1-2.jpg"), binary_img)
+
+        # 1.3 Convert the color image to the index-color image
+        index_color_img = convert_to_index_color(img)
+        cv2.imwrite(os.path.join(results_dir, filename[:-4] + "_q1-3.jpg"), index_color_img)
 
         # 2.1 Resize the image without interpolation
         double_img = resize_without_interpolation(img, 2)
